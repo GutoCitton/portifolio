79ec9f8a9442411fff980aec5c5d04cf398f1252
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2024-10-01 17:18:43 -0300

    implementando link whatsapp

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -67,7 +67,7 @@ function SimpleDialog(props: SimpleDialogProps) {
         <ListItem>
           <WhatsApp />
           <Typography margin={"0 0 0 8px"} fontSize={"17px"}>
-            <a href="https://wa.me/555196968550" target="blank">(51) 99696-8550</a>
+            <StyledLink href="https://wa.me/555196968550" target="blank">(51) 99696-8550</StyledLink>
           </Typography>
         </ListItem>
         <StyledLink
