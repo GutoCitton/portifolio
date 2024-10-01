aebda9495586dfea95bec922b4246031ae65409e
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2024-10-01 17:07:00 -0300

    implementando link whatsapp

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,7 +9,7 @@ import { Box, List, ListItem, styled } from "@mui/material";
 import { WhatsApp } from "@mui/icons-material";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import { AnimatedBackground } from "../AnimatedBackground/AnimatedBackground";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from "@mui/icons-material/GitHub";
 import theme from "../../theme";
 
 export interface SimpleDialogProps {
@@ -25,67 +25,69 @@ function SimpleDialog(props: SimpleDialogProps) {
   };
 
   const CustomDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiPaper-root': {
-      backgroundColor: 'rgb(20, 20, 20)',
-      alignItems: 'center',
-      color: 'white',
-      border: `solid 4px ${theme.palette.primary.main}`
+    "& .MuiPaper-root": {
+      backgroundColor: "rgb(20, 20, 20)",
+      alignItems: "center",
+      color: "white",
+      border: `solid 4px ${theme.palette.primary.main}`,
     },
-    backgroundColor: 'rgb(20, 20, 20,0.9)',
+    backgroundColor: "rgb(20, 20, 20,0.9)",
   }));
 
   const CustomDialogTitle = styled(DialogTitle)(() => ({
     alignItems: "center",
     color: "white",
-    fontWeight: 'bold',
+    fontWeight: "bold",
   }));
 
-  const StyledLink = styled('a')({
-    color: 'inherit',
-    textDecoration: 'none',
-    '&:visited': {
-      color: 'inherit'
+  const StyledLink = styled("a")({
+    color: "inherit",
+    textDecoration: "none",
+    "&:visited": {
+      color: "inherit",
     },
-    '&:hover': {
-      color: theme.palette.secondary.light
+    "&:hover": {
+      color: theme.palette.secondary.light,
     },
   });
 
   return (
     <CustomDialog onClose={handleClose} open={open}>
-      <Box position='absolute' width= '100%' top={-1} >
+      <Box position="absolute" width="100%" top={-1}>
         <AnimatedBackground />
       </Box>
       <CustomDialogTitle variant="h4">Contatos</CustomDialogTitle>
       <List>
         <ListItem>
           <AlternateEmailIcon />
-          <Typography margin={'0 0 0 8px'} fontSize={'17px'}>
+          <Typography margin={"0 0 0 8px"} fontSize={"17px"}>
             augusto.citton@gmail.com
           </Typography>
         </ListItem>
         <ListItem>
           <WhatsApp />
-          <Typography margin={'0 0 0 8px'} fontSize={'17px'}>  
-            (51) 99696-8550
+          <Typography margin={"0 0 0 8px"} fontSize={"17px"}>
+            <a href="https://wa.me/51996968550" target="blank">(51) 99696-8550</a>
           </Typography>
         </ListItem>
-        <StyledLink href="https://www.linkedin.com/in/augusto-ludwig/" target="blank">
+        <StyledLink
+          href="https://www.linkedin.com/in/augusto-ludwig/"
+          target="blank"
+        >
           <ListItem>
             <LinkedInIcon />
-            <Typography margin={'0 0 0 8px'} fontSize={'17px'}> 
+            <Typography margin={"0 0 0 8px"} fontSize={"17px"}>
               Augusto Ludwig
             </Typography>
           </ListItem>
         </StyledLink>
-        <StyledLink href="https://github.com/GutoCitton" target="blank" >
+        <StyledLink href="https://github.com/GutoCitton" target="blank">
           <ListItem>
             <GitHubIcon />
-            <Typography margin={'0 0 0 8px'} fontSize={'17px'} > 
+            <Typography margin={"0 0 0 8px"} fontSize={"17px"}>
               GitHub
             </Typography>
           </ListItem>
-          
         </StyledLink>
       </List>
     </CustomDialog>
